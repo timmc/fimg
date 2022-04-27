0eed2e37ce3cf0428908e4836c65316ce9d8cc13
Tim McCormack <cortex@brainonfire.net>
2022-04-26 23:18:52 -0400

    Roll phase or amplitude independently

--- fimg.py
+++ fimg.py
@@ -20,18 +20,42 @@ def save_image_grayscale(image_data):
     skimage.io.imsave('out.png', out_image_data)
 
 
+def roll(image, x, y):
+    image = numpy.roll(image, y, (0,))
+    image = numpy.roll(image, x, (1,))
+    return image
+
+
+def spatial_to_freq(spatial):
+    return fft.fft2(spatial)
+
+
+def freq_to_spatial(freq, shape):
+    return fft.irfft2(freq, shape)
+
+
+def freq_to_amp_phase(freq):
+    amplitude = abs(freq)
+    phase = numpy.angle(freq)
+    return amplitude, phase
+
+
+def amp_phase_to_freq(amplitude, phase):
+    real = numpy.cos(phase) * amplitude
+    imag = numpy.sin(phase) * amplitude
+    return real + imag * complex(0, 1)
+
+
 def main(roll_x, roll_y):
-    roll_x = int(roll_x)
-    roll_y = int(roll_y)
+    orig_image = load_image_grayscale()
+    orig_freq = spatial_to_freq(orig_image)
+    amplitude, phase = freq_to_amp_phase(orig_freq)
 
-    orig = load_image_grayscale()
+#    amplitude = roll(amplitude, int(roll_x), int(roll_y))
+    phase = roll(phase, int(roll_x), int(roll_y))
 
-    fourier = fft.fft2(orig)
-    fourier = numpy.roll(fourier, roll_y, (0,))
-    fourier = numpy.roll(fourier, roll_x, (1,))
-    image_i = fft.irfft2(fourier, orig.shape)
-
-    save_image_grayscale(image_i)
+    recomp_freq = amp_phase_to_freq(amplitude, phase)
+    save_image_grayscale(freq_to_spatial(recomp_freq, orig_image.shape))
 
 
 if __name__ == '__main__':
