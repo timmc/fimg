c802c46859e3baf6cd9842439bcc734158994252
Tim McCormack <cortex@brainonfire.net>
2022-05-03 22:59:31 -0400

    Test rescale

--- test.py
+++ test.py
@@ -12,3 +12,8 @@ class MathTests(unittest.TestCase):
         amp, phase = fimg.freq_to_amp_phase(freq)
         self.assertAlmostEqual(amp[0], math.sqrt(61), places=3)
         self.assertAlmostEqual(phase[0], 0.87606, places=3)
+
+    def test_rescale(self):
+        inarr = np.array([-9, 6, 21])
+        outarr = fimg.rescale(inarr, -9, 21, 0, 255)
+        self.assertEqual(outarr.tolist(), [0, 127.5, 255])
